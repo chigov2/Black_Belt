fef9aaef526601eeb1a65e2152a551bcf28ce0d2
chigov2 <chigov@gmail.com>
2023-06-28 16:20:27 +0300

    collection2

--- src/main/java/Collections/ArrayListEx1.java
+++ src/main/java/Collections/ArrayListEx1.java
@@ -5,5 +5,9 @@ import java.util.ArrayList;
 public class ArrayListEx1 {
     public static void main(String[] args) {
         ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Mike");
+        arrayList.add("Lena");
+        arrayList.add("Max");
+        arrayList.add("Alex");
     }
 }
