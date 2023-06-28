e1cc72ab8da8e5e8be847ed7c108bb0765557a62
User01 <chigov@gmail.cpm>
2023-06-28 16:03:10 +0300

    collections

---
+++ src/main/java/Collections/ArrayListEx1.java
@@ -0,0 +1,9 @@
+package main.java.Collections;
+
+import java.util.ArrayList;
+
+public class ArrayListEx1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+    }
+}
