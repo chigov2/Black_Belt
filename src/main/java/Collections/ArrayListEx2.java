a5538e2d5abb1a1691c0f41181023eb22388810c
User01 <chigov@gmail.cpm>
2023-06-29 12:29:43 +0300

    collections233

--- src/main/java/Collections/ArrayListEx2.java
+++ src/main/java/Collections/ArrayListEx2.java
@@ -2,3 +2,29 @@ package main.java.Collections;
 
 public class ArrayListEx2 {
 }
+class Student{
+    private String name;
+    private char sex;
+    private int age;
+    private int course;
+    private double avrGrade;
+
+    public Student(String name, char sex, int age,int course, double avrGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avrGrade = avrGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avrGrade=" + avrGrade +
+                '}';
+    }
+}
