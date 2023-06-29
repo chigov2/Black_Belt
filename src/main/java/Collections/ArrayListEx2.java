5db2b9783a1113691086771bd7811ca54d23933e
chigov2 <chigov@gmail.com>
2023-06-29 13:28:46 +0300

    collection2addall

--- src/main/java/Collections/ArrayListEx2.java
+++ src/main/java/Collections/ArrayListEx2.java
@@ -1,6 +1,29 @@
 package main.java.Collections;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class ArrayListEx2 {
+    public static void main(String[] args) {
+        Student st1 = new Student("Max",'m',15,1,5.8);
+        Student st2 = new Student("Mike",'m',50,4,6.8);
+        Student st3 = new Student("Lena",'f',45,5,4.8);
+        Student st4 = new Student("Alla",'f',60,3,5.2);
+        Student st5 = new Student("Victor",'m',30,2,5.0);
+
+        ArrayList<Student> studentArrayList = new ArrayList<>();
+        studentArrayList.add(st1);
+        studentArrayList.add(st2);
+        studentArrayList.add(st3);
+        studentArrayList.add(st4);
+        studentArrayList.add(st5);
+//        studentArrayList.add(st6);
+        System.out.println(studentArrayList);
+        Student st6 = new Student("Victor",'m',30,2,5.0);
+
+        studentArrayList.remove(st6);
+        System.out.println(studentArrayList);
+    }
 }
 class Student{
     private String name;
@@ -27,4 +50,17 @@ class Student{
                 ", avrGrade=" + avrGrade +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return sex == student.sex &&
+                age == student.age &&
+                course == student.course &&
+                Double.compare(student.avrGrade, avrGrade) == 0 &&
+                Objects.equals(name, student.name);
+    }
+
 }
---
+++ src/main/java/Collections/ArrayListEx3.java
@@ -0,0 +1,19 @@
+package main.java.Collections;
+
+import java.util.ArrayList;
+
+public class ArrayListEx3 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList3 = new ArrayList<>();
+        arrayList3.add("Mike");
+        arrayList3.add("Lena");
+        arrayList3.add("Max");
+        arrayList3.add("Alex");
+        arrayList3.add(1,"Vova");
+        System.out.println(arrayList3);
+
+        ArrayList<String> arrayList4 = new ArrayList<>();
+        arrayList3.add("111");
+        arrayList3.add("222");
+    }
+}
