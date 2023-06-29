a2f46dff99041225479d56817349a54c2a329bf0
chigov2 <chigov@gmail.com>
2023-06-29 11:34:26 +0300

    collection2_2

--- src/main/java/Collections/ArrayListEx1.java
+++ src/main/java/Collections/ArrayListEx1.java
@@ -9,7 +9,22 @@ public class ArrayListEx1 {
         arrayList.add("Lena");
         arrayList.add("Max");
         arrayList.add("Alex");
-        System.out.println(arrayList);
-        System.out.println(arrayList);
+        arrayList.add(1,"Vova");
+
+        for (String s: arrayList){
+            System.out.println(s);
+        }
+        System.out.println("--");
+
+        arrayList.set(2,"Alina");
+        arrayList.remove(0);
+//        for (int i= 0; i <arrayList.size();i++){
+//            System.out.println(arrayList.get(i));
+//        }
+        for (String s: arrayList){
+            System.out.println(s);
+        }
+        System.out.println("--");
+
     }
 }
---
+++ src/main/java/Collections/ArrayListEx2.java
@@ -0,0 +1,4 @@
+package main.java.Collections;
+
+public class ArrayListEx2 {
+}
