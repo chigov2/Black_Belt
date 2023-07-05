83bcfe2dc138642c28f822f943c915a0082ab4dc
chigov2 <86004573+chigov2@users.noreply.github.com>
2023-07-05 18:47:49 +0300

    21

--- src/main/java/Generics/ParameterizedClass2.java
+++ src/main/java/Generics/ParameterizedClass2.java
@@ -2,18 +2,18 @@ package main.java.Generics;
 
 public class ParameterizedClass2 {
     public static void main(String[] args) {
-        Pair1<String,Integer> pair1 = new Pair1<>("String value1",5);
-        System.out.println("parameters of pair1: "+ pair1.getFirstValue()+ " and " +pair1.getSecondValue());
-    Pair1<Integer,Double> pair2 = new Pair1<>(5,3.2);
-        System.out.println("parameters of pair2: "+ pair2.getFirstValue()+ " and " +pair2.getSecondValue());
+        Pair1<String, Integer> pair1 = new Pair1<>("String value1", 5);
+        System.out.println("parameters of pair1: " + pair1.getFirstValue() + " and " + pair1.getSecondValue());
+        Pair1<Integer, Double> pair2 = new Pair1<>(5, 3.2);
+        System.out.println("parameters of pair2: " + pair2.getFirstValue() + " and " + pair2.getSecondValue());
 
         System.out.println("----------");
-        Pair2<String> pair22 = new Pair2<>("first","Second");
-        System.out.println(pair22.getFirstValue() +" " + pair22.getSecondValue());
+        Pair2<String> pair22 = new Pair2<>("first", "Second");
+        System.out.println(pair22.getFirstValue() + " " + pair22.getSecondValue());
     }
 }
 
-class Pair1<V1,V2>{
+class Pair1<V1, V2> {
     private V1 value1;
     private V2 value2;
 
@@ -22,14 +22,16 @@ class Pair1<V1,V2>{
         this.value2 = value2;
     }
 
-    public V1 getFirstValue(){
+    public V1 getFirstValue() {
         return value1;
     }
-    public V2 getSecondValue(){
+
+    public V2 getSecondValue() {
         return value2;
     }
 }
-class Pair2<V>{
+
+class Pair2<V> {
     private V value1;
     private V value2;
 
@@ -38,14 +40,15 @@ class Pair2<V>{
         this.value2 = value2;
     }
 
-    public V abc(V value){//<V>  здесь не надо
+    public V abc(V value) {//<V>  здесь не надо
         return value;
     }
 
-    public V getFirstValue(){
+    public V getFirstValue() {
         return value1;
     }
-    public V getSecondValue(){
+
+    public V getSecondValue() {
         return value2;
     }
 }
