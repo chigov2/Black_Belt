266ad63e53b2f78d2de9e15d73b3c1afe46843ce
chigov2 <chigov@gmail.com>
2023-07-06 09:56:41 +0300

    LinkedHashmapExample1

---
+++ src/main/java/Collections/MapInterface/LinkedHashmapExample1.java
@@ -0,0 +1,24 @@
+package main.java.Collections.MapInterface;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashmapExample1 {
+    public static void main(String[] args) {
+        LinkedHashMap<Double,Student> linkedHashMap = new LinkedHashMap<>();
+        Student student1 = new Student("Mike","Stoba", 4);
+        Student student2 = new Student("Max","Stoba", 3);
+        Student student3 = new Student("Lena","Gerasymliuk", 5);
+        Student student4 = new Student("Igor","Kol", 4);
+        Student student5 = new Student("Anna","Kovalchuk", 3);
+        Student student6 = new Student("Alex","Gerasymliuk", 5);
+        Student student7 = new Student("Alla","Gerasymliuk", 5);
+        linkedHashMap.put(3.7,student5);
+        linkedHashMap.put(4.7,student6);
+        linkedHashMap.put(5.7,student4);
+        linkedHashMap.put(6.7,student3);
+        linkedHashMap.put(7.7,student2);
+        linkedHashMap.put(8.7,student1);
+        linkedHashMap.put(9.7,student7);
+        System.out.println(linkedHashMap);
+    }
+}
