a68ebe7c35dbbe69bc129c3149992baa3c9b2fa1
chigov2 <chigov@gmail.com>
2023-07-12 13:37:59 +0300

    LocalInnerClass1.java

---
+++ src/main/java/NestedClasses/LocalInnerClasses/LocalInnerClass1.java
@@ -0,0 +1,45 @@
+package main.java.NestedClasses.LocalInnerClasses;
+
+public class LocalInnerClass1 {
+    public static void main(String[] args) {
+        Math math = new Math();
+        math.getResult();
+    }
+}
+
+class Math{
+    public void getResult(){
+        class Delenie{
+            private int delimoe;
+            private int delitel;
+
+            public int getDelimoe() {
+                return delimoe;
+            }
+            public void setDelimoe(int delimoe) {
+                this.delimoe = delimoe;
+            }
+            public int getDelitel() {
+                return delitel;
+            }
+            public void setDelitel(int delitel) {
+                this.delitel = delitel;
+            }
+
+            public  int getChastnoe(){
+                return delimoe/delitel;
+            }
+            public  int getOstatok(){
+                return delimoe % delitel;
+            }
+
+        }
+        Delenie delenie = new Delenie();
+        delenie.setDelimoe(8);
+        delenie.setDelitel(3);
+        System.out.println("Delimoe: "+ delenie.getDelimoe());
+        System.out.println("Delitel: "+ delenie.getDelitel());
+        System.out.println("Chastnoe: "+ delenie.getChastnoe());
+        System.out.println("Ostatok: "+ delenie.getOstatok());
+    }
+}
