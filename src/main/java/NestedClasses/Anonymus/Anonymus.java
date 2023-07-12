e93c59332970b1d43cb274660c811864f30fdde2
chigov2 <chigov@gmail.com>
2023-07-12 15:58:28 +0300

    Anonymus

--- src/main/java/NestedClasses/Anonymus/Anonymus.java
+++ src/main/java/NestedClasses/Anonymus/Anonymus.java
@@ -1,6 +1,7 @@
 package main.java.NestedClasses.Anonymus;
 
 public class Anonymus {
+    private  int с = 5;
     public static void main(String[] args) {
         Math m = new Math() {
             @Override
@@ -8,6 +9,16 @@ public class Anonymus {
                 return a - b;
             }
         };
+
+        Math m2 = new Math() {
+            @Override
+            public int doOperation(int a, int b) {
+                Anonymus ac = new Anonymus();
+                return a * b + ac.с;
+            }
+        };
+        System.out.println(m.doOperation(6,3));
+        System.out.println(m2.doOperation(6,3));
     }
 }
 
