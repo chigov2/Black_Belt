470e4b23ba88bb8c3585f3e64d724949eeec93e9
mikee <chigov@gmail.com>
2023-07-20 15:35:18 +0300

    concat

--- src/main/java/Stream/Terminal6.java
+++ src/main/java/Stream/Terminal6.java
@@ -1,5 +1,7 @@
 package main.java.Stream;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -9,7 +11,13 @@ public class Terminal6 {
         stream1.filter(element -> {
             System.out.println("-------");
             return element%2 == 0;
-        }).collect(Collectors.toList())
-        ;
+        }).collect(Collectors.toList());
+        System.out.println("---2---");
+        Stream<Integer> stream2 = Stream.of(1,4,3,4,5);
+        Stream<Integer> stream3 = Stream.of(6,7,8,9,10);
+        Stream<Integer> stream4 = Stream.concat(stream2,stream3);
+        stream4.forEach(el-> System.out.println(el));
     }
+
+
 }
