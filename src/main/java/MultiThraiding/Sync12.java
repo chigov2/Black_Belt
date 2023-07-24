831b21a6372858c9534a204091e58d2bc624274a
mikee <chigov@gmail.com>
2023-07-24 15:37:28 +0300

    lock

--- src/main/java/MultiThraiding/Sync12.java
+++ src/main/java/MultiThraiding/Sync12.java
@@ -1,4 +1,77 @@
 package main.java.MultiThraiding;
 
 public class Sync12 {
+//    static final ToSync toSync = new ToSync();
+    static final Object lock = new Object();
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new RunnableImplMobile());
+        Thread thread2 = new Thread(new RunnableImplSkype());
+        Thread thread3 = new Thread(new RunnableImplWhatsUp());
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+//    synchronized void mobileCall() {
+        void mobileCall() {
+        synchronized (lock){
+//        System.out.println(this);
+        System.out.println("Mobile call starts");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Mobile call ends");}
+    }
+
+    void skypeCall() {
+        synchronized (lock) {
+//            System.out.println(this);
+            System.out.println("Skype call starts");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Skype call ends");
+        }
+    }
+
+    void whatsAppCall() {
+        synchronized (lock) {
+//            System.out.println(this);
+            System.out.println("WhatsApp call starts");
+            try {
+                Thread.sleep(7000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("WhatsApp call ends");
+        }
+    }
 }
+
+class RunnableImplMobile implements Runnable {
+    @Override
+    public void run() {
+        new Sync12().mobileCall();
+    }
+}
+
+class RunnableImplSkype implements Runnable {
+    @Override
+    public void run() {
+        new Sync12().skypeCall();
+        ;
+    }
+}
+
+class RunnableImplWhatsUp implements Runnable {
+    @Override
+    public void run() {
+        new Sync12().whatsAppCall();
+    }
+}
+
+//class  ToSync{}
