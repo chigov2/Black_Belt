c433cae62c3f0326164b03f695c811ebf0d46ea3
chigov2 <chigov@gmail.com>
2023-07-24 10:04:09 +0300

    multithraiding3

---
+++ src/main/java/MultiThraiding/Sync1.java
@@ -0,0 +1,32 @@
+package main.java.MultiThraiding;
+
+public class Sync1 {
+    public static void main(String[] args) {
+        MyRunnableEx2 runnable = new MyRunnableEx2();
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        Thread thread3 = new Thread(runnable);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+
+}
+class Counter{
+    static int count = 0;
+}
+
+class MyRunnableEx2 implements Runnable{
+    public void counterFunc(){
+        Counter.count++;
+        System.out.print(Counter.count + " ");
+    }
+    @Override
+    public void run() {
+        for (int i =0; i < 3; i++){
+            counterFunc();
+        }
+
+    }
+}
