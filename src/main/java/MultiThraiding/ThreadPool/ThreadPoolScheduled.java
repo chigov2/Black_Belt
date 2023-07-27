7c119c126d51df5749d701ece9f8a0680f45a550
mikee <chigov@gmail.com>
2023-07-27 15:22:02 +0300

    Scheduled22

--- src/main/java/MultiThraiding/ThreadPool/ThreadPoolScheduled.java
+++ src/main/java/MultiThraiding/ThreadPool/ThreadPoolScheduled.java
@@ -7,8 +7,10 @@ public class ThreadPoolScheduled {
 class RunnableImpl200 implements Runnable{
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName() +" begin works");
         try {Thread.sleep(500);}
         catch (InterruptedException e) {e.printStackTrace();}
+        System.out.println(Thread.currentThread().getName() +" stop works");
     }
+
 }
