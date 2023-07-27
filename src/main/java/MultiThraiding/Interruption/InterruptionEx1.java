293693b45a75a43cb50e857845d0d12d3cca95bb
mikee <chigov@gmail.com>
2023-07-27 12:30:03 +0300

    DaemonExample1

---
+++ src/main/java/MultiThraiding/Interruption/InterruptionEx1.java
@@ -0,0 +1,38 @@
+package main.java.MultiThraiding.Interruption;
+
+public class InterruptionEx1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Main starts");
+        InterruptedThread thread = new InterruptedThread();
+        thread.start();
+        Thread.sleep(2000);//main thread sleep
+        thread.interrupt();
+
+        thread.join();
+        System.out.println("Main ends");
+
+    }
+}
+
+class InterruptedThread extends Thread{
+    double sqrtSum = 0;
+    @Override
+    public void run() {
+        for (int i = 0; i < 2000_000_000; i++){
+            if (isInterrupted()){
+                System.out.println("Поток хотят прервать");
+                System.out.println("Мы убедились, что состояние всех пакетов нормальное и решили завершить работу потока");
+                System.out.println(sqrtSum);
+                return;
+            }
+            sqrtSum += Math.sqrt(i);
+            try {sleep(1000);}
+            catch (InterruptedException e) {
+                System.out.println("Поток хотят прервать во время сна");
+                System.out.println(sqrtSum);
+                return;
+            }
+        }
+        System.out.println(sqrtSum);
+    }
+}
