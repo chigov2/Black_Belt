a50fdcaf590662acca1f1d0669248c52b103dd27
mikee <chigov@gmail.com>
2023-08-02 15:15:41 +0300

    SynchronizedCollections

---
+++ src/main/java/ThreadSafe/SynchronizedCollections.java
@@ -0,0 +1,31 @@
+package main.java.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SynchronizedCollections {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<Integer> source = new ArrayList<>();
+
+        for (int i =0; i<5; i++){
+            source.add(i);
+        }
+//        ArrayList<Integer> target = new ArrayList<>();
+        ArrayList<Integer> target = new ArrayList<>();
+//        List<Integer> syncList = Collections.synchronizedList(new ArrayList<>());
+        List<Integer> syncList = Collections.synchronizedList(target);
+
+        Runnable runnable = ()-> syncList.addAll(source);
+
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+        System.out.println(syncList);
+    }
+}
---
+++ src/main/java/ThreadSafe/SynchronizedCollectionsEx2.java
@@ -0,0 +1,4 @@
+package main.java.ThreadSafe;
+
+public class SynchronizedCollectionsEx2 {
+}
