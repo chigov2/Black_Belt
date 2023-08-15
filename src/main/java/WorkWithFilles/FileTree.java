f10503e34f0774c001271cfd52f14d2df708e3c1
1111 <chigov@gmail.com>
2023-08-15 20:12:03 +0300

    WalkFileTree

---
+++ src/main/java/WorkWithFilles/FileTree.java
@@ -0,0 +1,40 @@
+package main.java.WorkWithFilles;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileTree {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:\\Users\\1111\\Desktop\\X");
+
+        Files.walkFileTree(path, new MyFileVisitor());
+    }
+}
+
+class MyFileVisitor implements FileVisitor<Path>{
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println("Enter to directory: " + dir);
+        return  FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println("Filemane: " + file.getFileName());
+        return  FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error while visiting file: " + file);
+        return FileVisitResult.TERMINATE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("Exit from directory: " + dir);
+        return FileVisitResult.CONTINUE;
+    }
+}
