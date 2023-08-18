1411cda1ef43dbbd861b0ae4d9cd91ca7ccac411
mikee <chigov@gmail.com>
2023-08-18 11:24:40 +0300

    Enum

--- src/main/java/Enums/Unum1.java
+++ src/main/java/Enums/Unum1.java
@@ -1,14 +1,62 @@
 package main.java.Enums;
 
+import java.util.Arrays;
+
 public class Unum1 {
     public static void main(String[] args) {
         Today today = new Today(WeekDays.MONDAY);
         today.daysInfo();
+        System.out.println(today.weekDay);
+        WeekDays w1 = WeekDays.FRIDAY;
+        WeekDays w2 = WeekDays.FRIDAY;
+        WeekDays w3 = WeekDays.MONDAY;
+        //сравнение
+        System.out.println(w1==w2);
+        System.out.println(WeekDays.FRIDAY.equals(WeekDays2.FRIDAY));
+        System.out.println("------valueOf-----");
+        WeekDays w16 = WeekDays.valueOf("FRIDAY");
+        System.out.println(w16);
+        System.out.println("-----values------");
+        WeekDays [] array = WeekDays.values();
+        System.out.println(Arrays.toString(array));
+        System.out.println("-----values2------");
+        for (WeekDays i: array){
+            System.out.println(i);
+        }
     }
 }
 
 enum WeekDays{
-    SUNDAY,MONDAY, TUESDAY, WEDNESDAY,THURSDAY,FRIDAY,SATURDAY;
+    SUNDAY("Good"),
+    MONDAY("Bad"),
+    TUESDAY("Close to normal"),
+    WEDNESDAY("Middle!!!"),
+    THURSDAY(),
+    FRIDAY("OK!!"),
+    SATURDAY("Very good");
+
+    private String mood;
+
+    WeekDays(String mood) {
+        this.mood = mood;
+    }
+
+    WeekDays() {
+    }
+
+    String getMood(){
+        return mood;
+    }
+}
+
+enum WeekDays2{
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY;
 }
 
 class Today{
@@ -32,5 +80,6 @@ class Today{
                 System.out.println("Take a rest");
                 break;
         }
+        System.out.println("Mood today: " + weekDay.getMood());
     }
 }
