73487d4870ca6085650bb09674ccefac5266d49b
mikee <chigov@gmail.com>
2023-09-29 09:59:08 +0300

    Finance01 ListFiltering

---
+++ src/main/java/Kata/Practeice/Test/ListFiltering.java
@@ -0,0 +1,28 @@
+package main.java.Kata.Practeice.Test;
+
+import java.util.List;
+
+//In this kata you will create a function that takes a list
+//        of non-negative integers and strings and returns
+//        a new list with the strings filtered out.
+
+//Kata.filterList(List.of(1, 2, "a", "b")) => List.of(1,2)
+//        Kata.filterList(List.of(1, 2, "a", "b", 0, 15)) => List.of(1,2,0,15)
+//        Kata.filterList(List.of(1, 2, "a", "b", "aasf", "1", "123", 231)) => List.of(1, 2, 231)
+
+//if (new Scanner(string).hasNextInt()) {
+//        int number = Integer.parseInt(string);
+//        }
+
+// isNumeric
+// parse
+
+public class ListFiltering {
+    public static void main(String[] args) {
+
+    }
+    public static List<Object> filterList(final List<Object> list) {
+        // Return the List with the Strings filtered out
+        return null;
+    }
+}
