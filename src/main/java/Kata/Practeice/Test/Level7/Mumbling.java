a47f143387bb744745c75df2999d0e5bae46ba71
mikee <chigov@gmail.com>
2023-10-14 17:06:41 +0300

    7 level878

--- src/main/java/Kata/Practeice/Test/Level7/Mumbling.java
+++ src/main/java/Kata/Practeice/Test/Level7/Mumbling.java
@@ -2,12 +2,43 @@ package main.java.Kata.Practeice.Test.Level7;
 
 public class Mumbling {
     public static void main(String[] args) {
-        String s = "cwAt";
+        String s = "abcde";
         accum(s);
     }
 
     public static String accum(String s) {
-        // your code
-        return "";
+        StringBuilder builder = new StringBuilder();
+        StringBuilder builder1 = new StringBuilder();
+        String s2 = "";
+
+        String[] split = s.split("");
+
+
+        for (int i = 0; i < split.length; i++) {
+            builder.append(split[i]);
+
+            for (int j = 0; j < i; j++) {
+                builder.append(split[i]);
+            }
+
+            if (i < split.length - 1) {
+                builder.append("-");
+            }
+        }
+        System.out.println(builder.toString());
+
+
+        return builder.toString();
     }
+
 }
+
+
+
+
+
+
+
+
+
+
